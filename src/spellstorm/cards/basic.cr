8ae852c79ee2e463dde9c9dddcdbe8e67e846362
Your Name <you@example.com>
2016-10-02 19:15:46 +0000

    test card names

---
+++ src/spellstorm/cards/basic.cr
@@ -0,0 +1,45 @@
+require "../cards.cr"
+
+module Spellstorm
+
+class ShieldCard < Card
+
+  def typ_name : String
+    "Щит"
+  end
+
+end
+
+class DangerCard < Card
+
+  def typ_name : String
+    "Угроза"
+  end
+
+end
+
+class ActionCard < Card
+
+  def typ_name : String
+    "Действие"
+  end
+
+end
+
+class EnchantCard < Card
+
+  def typ_name : String
+    "Чары"
+  end
+
+end
+
+class SourceCard < Card
+
+  def typ_name : String
+    "Накопитель"
+  end
+
+end
+
+end
--- src/spellstorm/cards/testset.cr
+++ src/spellstorm/cards/testset.cr
@@ -1,15 +1,8 @@
 
 require "../cards.cr"
+require "./basic.cr"
 
 module Spellstorm
-
-class ShieldCard < Card
-
-  def typ_name : String
-    "Щит"
-  end
-
-end
 
 class CardsDB
   getter data : Array(Card)
@@ -18,6 +11,19 @@ class CardsDB
     @data = [] of Card
     #@name, @cost, @element, @power
     @data << ShieldCard.new("Малый щит",2,Element::Neutral,2)
+    @data << ShieldCard.new("Рассеивающий щит",5,Element::Neutral,2)
+    @data << ShieldCard.new("Большой щит",6,Element::Neutral,5)
+    @data << DangerCard.new("Малая угроза",1,Element::Neutral,1)
+    @data << DangerCard.new("Средняя угроза",3,Element::Neutral,2)
+    @data << DangerCard.new("Большая угроза",7,Element::Neutral,2)
+    @data << ActionCard.new("Молния",2,Element::Neutral,2)
+    @data << ActionCard.new("Большая Молния",5,Element::Neutral,5)
+    @data << EnchantCard.new("Тишина",3,Element::Neutral,2)
+    @data << EnchantCard.new("Изоляция",3,Element::Neutral,1)
+    @data << EnchantCard.new("Противофаза",1,Element::Neutral,0)
+    @data << EnchantCard.new("Рассеивание",3,Element::Neutral,0)
+    @data << SourceCard.new("Малый резервуар",2,Element::Neutral,2)
+    @data << SourceCard.new("Большой резервуар",5,Element::Neutral,5)
   end
 
   def sample : Card
