9e7b92e795f666b8424f57c608de43e727dc539d
Your Name <you@example.com>
2016-10-08 23:43:36 +0000

    started specs for mechanics. somehow they fails

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,2 +1,2 @@
 require "spec"
-require "../src/spellstorm"
+require "../src/spellstorm/*"
--- spec/spellstorm_spec.cr
+++ spec/spellstorm_spec.cr
@@ -1,9 +1,21 @@
 require "./spec_helper"
 
-describe Spellstorm do
-  # TODO: Write tests
+include Spellstorm
 
-  it "works" do
-    false.should eq(true)
+describe Spellstorm do
+
+  decks = {Deck.new, Deck.new}
+  it "decks generation" do
+    decks.each &.generate
+    decks.each {|deck| deck.data.size.should eq(DECK_SIZE)}
   end
+  game_state = GameState.new(decks)
+  it "new game" do
+    game_state.parts.each do |part|
+      part.hp.should eq(MAX_HP)
+      part.count_cards(CardLocation::Deck).should eq(DECK_SIZE-MAX_HP)
+      part.count_cards(CardLocation::Hand).should eq(MAX_HP)
+    end
+  end
+
 end
